1f376f365ef75b8088e9b44edf0cdf67cd5e6e23
asadgjkl <147242857+asadgjkl@users.noreply.github.com>
2023-10-07 14:48:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('btn');
+var btn2 = document.getElementById('btn2');
+
+btn.onclick = function(){
+   var pr = prompt('matn kiriting')
+   p.innerHTML =pr
+}
+
+var btn2 = document.getElementById('btn2');
+btn2.onclick= function(){
+    p.innerHTML = ''}
